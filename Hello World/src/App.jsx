db5af46224850417bcf065773c7ecf5a2dbbc811
Sadat saeed <afarisamuel6@gmail.com>
2023-11-08 16:54:05 +0000

    this is my first react app

---
+++ Hello World/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>I am Samuel Sadat Saeed Afari, this is my first react application</p>
+    </>
+  )
+}
+
+export default App
